852e73a2d41974972c209d53e381787e966245ab
Hutch-Johnson <108013011+Hutch-Johnson@users.noreply.github.com>
2025-11-19 14:22:29 -0800

    More data for ProGen2

--- protein_data.py
+++ protein_data.py
@@ -10,6 +10,8 @@ import seaborn as sns
 import pandas as pd
 import pickle
 
+from scipy.spatial.distance import jensenshannon
+
 def llr_heatmap(llr_matrix, positions=None, figsize=(15, 10), 
                 cmap='RdBu_r',sequence='sequence'):
     '''
@@ -49,3 +51,39 @@ def pickle_plm_matrices(dict, filename):
     with open(filename, 'wb') as f:
         pickle.dump(dict, f, protocol=pickle.HIGHEST_PROTOCOL)
 
+
+def prob_like_js(prob_dist1, prob_dist2, threshold=0.5):
+    if jensenshannon(prob_dist1, prob_dist2) < threshold:
+        return True
+    return False
+
+
+def plot_dist(prob_list, plot_range=(0,50)):
+    eB = prob_list
+
+    Blist = [np.array(eB)[i] for i in range(plot_range[0],plot_range[1])]
+
+    cols = (plot_range[1] - plot_range[0] + 9)//10
+    fig, axes = plt.subplots(10, cols, figsize=(25, 25))
+    axes = axes.flatten()
+    bar_width = 0.4
+    x = torch.arange(1, 21)
+
+    for i, ax in enumerate(axes):
+        dist2 = Blist[i]
+        # ax.bar(x - bar_width/2, dist1, width=bar_width, label="Dist 1", color="skyblue")
+        ax.bar(x, dist2, width=bar_width, color="blue")
+        ax.set_title(f"Comparison {i+1}")
+        ax.set_xlabel("Amino Acids")
+        ax.set_ylabel("Probability")
+        ax.set_ylim(0, 1)
+        ax.set_xticks(x)
+        ax.set_xticklabels(aa_list)  
+        ax.tick_params(axis='x', rotation=0)
+
+    # Add a single legend for all subplots
+    handles, labels = axes[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper center', ncol=2)
+
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+    plt.show()
